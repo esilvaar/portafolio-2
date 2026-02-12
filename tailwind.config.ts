c88b0546d2d4a15e8ca2bd878ab12b1dc23038d7
esilvaar <eduardo20032110@gmail.com>
2026-02-12 16:00:13 +0000

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+// tailwind.config.ts
+const config = {
+  theme: {
+    extend: {
+      colors: {
+        background: '#050a15', // El azul oscuro profundo de tu imagen
+        primary: '#1d63ff',    // El azul brillante de los botones
+        surface: '#111827',    // Color de las tarjetas (Terminal/Stats)
+      },
+    },
+  },
+}
+export default config;
